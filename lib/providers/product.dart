f05202799046c8e3546d233d8e28049604056220
Minh Huy <hyheo1996@gmail.com>
2021-01-28 21:09:20 +0700

    Add authentication

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -25,17 +25,18 @@ class Product with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> toggleFavoriteStatus() async {
+  Future<void> toggleFavoriteStatus(String token, String userId) async {
     final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
-    final url = 'https://shop-app-flutter-71e1d-default-rtdb.firebaseio.com/$id.json';
+    final url =
+        'https://shop-app-flutter-71e1d-default-rtdb.firebaseio.com/$userId/$id.json?auth=$token';
     try {
-      final response = await http.patch(
+      final response = await http.put(
         url,
-        body: json.encode({
-          'isFavorite': isFavorite,
-        }),
+        body: json.encode(
+          isFavorite,
+        ),
       );
       if (response.statusCode >= 400) {
         _setFavValue(oldStatus);
